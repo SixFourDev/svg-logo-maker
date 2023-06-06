d4ff1df7e9aecfd649bdfd6153d649ce834ee661
Shane <sb_94@yahoo.com>
2023-06-06 11:05:26 -0700

    have user enter text color and logs the response

--- index.js
+++ index.js
@@ -7,23 +7,31 @@ function promptText() {
     return inquirer
       .prompt([
         {
-            // Specifies the prompt for the user
+            // Enter characters prompt
           type: 'input',
           name: 'text',
           message: 'Enter up to three characters:',
           validate: function (input) {
             // Validate that the input is up to three characters
-            if (input.length !== 3) {
+            if (input.length > 3) {
               return 'Please enter up to three characters.';
             }
             return true;
           },
         },
+        {
+        // Enter text color prompt
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the text color (keyword or hexadecimal number):'
+        },
       ])
       // Once promise is returned passes the response from user and logs the text
       .then((response) => {
         const text = response.text;
+        const textColor = response.textColor;
         console.log('Entered text:', text);
+        console.log('Entered text color:', textColor);
       });
   }
   
